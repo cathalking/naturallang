222b805008d2b606aa3de56a886551f63998aa30
Cathal King <cathalking@Cathals-MacBook-Air.local>
2026-03-06 20:55:17 +0000

    Stop tracking generated build artifacts

--- build/tmp/stubs/org/jsoup/nodes/Document.java
+++
@@ -1,9 +0,0 @@
-package org.jsoup.nodes;
-
-import org.jsoup.select.Elements;
-
-public class Document {
-    public Elements select(String ignoredCssQuery) {
-        return new Elements();
-    }
-}
